e88e816d2e3620d814f19c2efda17ef688e9c406
Kenji Miwa <kenjimiwa@Kenjis-MacBook-Pro.local>
2014-05-15 16:36:45 -0700

    merged all works

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,11 +1,17 @@
+# Home
+
 get '/' do
   erb :index
 end
 
 post '/user/login' do
   @user = User.find_by_username(params[:username])
-  session[:user_id] = @user.id
-  redirect "/user/#{@user.id}"
+  if @user && (@user.password == params[:password])
+    session[:user_id] = @user.id
+    redirect "/user/#{@user.id}"
+  else
+    redirect '/'
+  end
 end
 
 post '/user/new' do
@@ -14,20 +20,32 @@ post '/user/new' do
   redirect "/user/#{@user.id}"
 end
 
-post 'user/logout' do
+get '/logout' do
   session.clear
+  redirect '/'
 end
+
 
 get '/user/:id' do
+  if session[:user_id]
+    @userid = params[:id]
+    @user_name = User.find(@userid).name
+    @user_tweets = User.find(@userid).tweets
+    erb :profile
+  else
+    redirect '/'
+  end
 
-get '/info' do
-  Demo.new(self).info
 end
 
-get '/user/:id' do
-  @userid = params[:id]
-  @user_name = User.find(@userid).name
-  @user_tweets = User.find(@userid).tweets
 
-  erb :profile
+# Tweets
+
+post '/tweet/new' do
+  # Create new tweet per the user id
+  user_id = session[:user_id]
+  User.find(user_id).tweets.create(content: params[:tweet])
+  redirect "/user/#{user_id}"
 end
+
+
