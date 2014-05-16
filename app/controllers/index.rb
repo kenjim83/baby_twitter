0cd450ab8acce7dc7751ef95377721f0c2000e67
Ziad Abdo <apprentice@dbc11.local>
2014-05-16 16:26:28 -0700

    added some minor validation features. rackflash implemented for sign up/unique user---- need much more validation

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,5 +1,4 @@
 # Home
-
 get '/' do
   erb :index
 end
@@ -15,9 +14,17 @@ post '/user/login' do
 end
 
 post '/user/new' do
-  @user = User.create(params)
-  session[:user_id] = @user.id
-  redirect "/user/#{@user.id}"
+  @user = User.new(params)
+  if @user.valid?
+    @user.save
+    session[:user_id] = @user.id
+    flash[:name_taken] = false
+    redirect "/user/#{@user.id}"
+  else
+    flash[:name_taken] = true
+    redirect "/"
+  end
+
 end
 
 get '/logout' do
